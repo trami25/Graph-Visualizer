d6e0edb1cb79bfc85b43c4ff614eaf101d3d9d63
Nikola Mitrovic <mnikola300@gmail.com>
2024-02-04 12:10:35 +0100

    third version of visualizer plugin implemented

--- simple_visualizer_plugin/src/simple_visualizer.py
+++ simple_visualizer_plugin/src/simple_visualizer.py
@@ -1,3 +1,5 @@
+import os
+
 from jinja2 import Environment, FileSystemLoader
 from graph_visualizer_api.visualizer import Visualizer
 from graph_visualizer_api.model.graph import Graph
@@ -5,7 +7,10 @@ from graph_visualizer_api.model.graph import Graph
 
 class SimpleVisualizer(Visualizer):
     def generate_template(self, graph: Graph) -> str:
-        env = Environment(loader=FileSystemLoader("C:\\Users\\mniko\\PycharmProjects\\Graph-VisualizerSOK\\simple_visualizer_plugin\\template"))
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        template_path = os.path.join(current_dir, "template")
+
+        env = Environment(loader=FileSystemLoader(template_path))
         template = env.get_template("simple_visualizer_plugin/simple_visualizer_template.jinja2")
 
         html = template.render(graph=graph)
