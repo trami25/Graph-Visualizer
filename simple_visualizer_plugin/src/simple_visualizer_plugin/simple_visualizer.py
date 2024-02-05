aeadbe7d011a06f4d1ec7fb1897e7785d7cc9b21
Nikola Mitrovic <mnikola300@gmail.com>
2024-02-05 11:18:11 +0100

    fourth version of the plugin with d3 library implemented

--- simple_visualizer_plugin/src/simple_visualizer_plugin/simple_visualizer.py
+++ simple_visualizer_plugin/src/simple_visualizer_plugin/simple_visualizer.py
@@ -6,7 +6,21 @@ from graph_visualizer_api.model.graph import Graph
 
 
 class SimpleVisualizer(Visualizer):
+    """Generates HTML template for visualizing a graph using Jinja2.
+
+    This class is a plugin that implements the visualizer for the Graph Visualizer. It uses Jinja2 templates
+    to generate HTML for visualizing a graph.
+
+    Attributes:
+        None
+    """
+
     def generate_template(self, graph: Graph) -> str:
+        """Generates HTML template for the given graph.
+
+        :param graph: Graph to be visualized.
+        :return: HTML template as a string.
+        """
         current_dir = os.path.dirname(os.path.abspath(__file__))
         template_path = os.path.join(current_dir, "../template")
 
