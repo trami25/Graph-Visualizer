283c57fca9b6bce910277b279b77827a8386f1b8
Vladislav Radovic <vradovic522@gmail.com>
2024-02-07 18:27:12 +0100

    feat: load plugins

--- graph_visualizer_core/src/visualizer/apps.py
+++ graph_visualizer_core/src/visualizer/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from graph_visualizer_platform.plugins import PluginManager
 
 
 class VisualizerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'visualizer'
+    data_source_plugins = []
+    visualizer_plugins = []
+
+    def ready(self):
+        plugin_manager = PluginManager()
+        self.data_source_plugins = plugin_manager.data_sources
+        self.visualizer_plugins = plugin_manager.visualizers
--- graph_visualizer_core/src/visualizer/views.py
+++ graph_visualizer_core/src/visualizer/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.apps.registry import apps
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'visualizer/index.html')
+    data_source_plugins = apps.get_app_config('visualizer').data_source_plugins
+    visualizer_plugins = apps.get_app_config('visualizer').visualizer_plugins
+    return render(request, 'visualizer/index.html', context={'data_source_plugins': data_source_plugins, 'visualizer_plugins': visualizer_plugins})
