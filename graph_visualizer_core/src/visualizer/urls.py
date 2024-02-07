dc4a90bd95385bc6856eb7b83d879bc76a842d9c
Vladislav Radovic <vradovic522@gmail.com>
2024-02-07 19:04:05 +0100

    feat: raise 404 if workspace does not exist

--- graph_visualizer_core/src/visualizer/urls.py
+++ graph_visualizer_core/src/visualizer/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:tag>/", views.workspace, name="workspace"),
+    path("<str:tag>/", views.workspace_view, name="workspace"),
 ]
--- graph_visualizer_core/src/visualizer/views.py
+++ graph_visualizer_core/src/visualizer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.apps.registry import apps
+from django.http import Http404
+from graph_visualizer_platform.workspaces import WorkspaceManager
 
 
 # Create your views here.
@@ -15,14 +17,20 @@ def index(request):
                   })
 
 
-def workspace(request, tag):
+def workspace_view(request, tag):
     data_source_plugins = apps.get_app_config('visualizer').data_source_plugins
     visualizer_plugins = apps.get_app_config('visualizer').visualizer_plugins
     workspaces = apps.get_app_config('visualizer').workspaces
+
+    workspace_manager = WorkspaceManager()
+    active_workspace = workspace_manager.get_by_tag(tag)
+    if active_workspace is None:
+        raise Http404('Workspace does not exist.')
+
     return render(request, 'visualizer/workspace.html',
                   context={
                       'data_source_plugins': data_source_plugins,
                       'visualizer_plugins': visualizer_plugins,
                       'workspaces': workspaces,
-                      'tag': tag
+                      'active_workspace': active_workspace
                   })
