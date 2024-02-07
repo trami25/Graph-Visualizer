8a5cca14f630e4dd16791707cf4d1954d970917e
Vladislav Radovic <vradovic522@gmail.com>
2024-02-07 18:54:08 +0100

    feat: add individual workspaces

--- graph_visualizer_core/src/visualizer/urls.py
+++ graph_visualizer_core/src/visualizer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:tag>/", views.workspace, name="workspace"),
 ]
--- graph_visualizer_core/src/visualizer/views.py
+++ graph_visualizer_core/src/visualizer/views.py
@@ -7,9 +7,22 @@ def index(request):
     data_source_plugins = apps.get_app_config('visualizer').data_source_plugins
     visualizer_plugins = apps.get_app_config('visualizer').visualizer_plugins
     workspaces = apps.get_app_config('visualizer').workspaces
-    return render(request, 'visualizer/index.html',
+    return render(request, 'visualizer/base.html',
                   context={
                       'data_source_plugins': data_source_plugins,
                       'visualizer_plugins': visualizer_plugins,
                       'workspaces': workspaces
+                  })
+
+
+def workspace(request, tag):
+    data_source_plugins = apps.get_app_config('visualizer').data_source_plugins
+    visualizer_plugins = apps.get_app_config('visualizer').visualizer_plugins
+    workspaces = apps.get_app_config('visualizer').workspaces
+    return render(request, 'visualizer/workspace.html',
+                  context={
+                      'data_source_plugins': data_source_plugins,
+                      'visualizer_plugins': visualizer_plugins,
+                      'workspaces': workspaces,
+                      'tag': tag
                   })
