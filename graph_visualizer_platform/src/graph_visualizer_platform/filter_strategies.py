198ce50390224647c58bd7c412e757942aefaef2
ZivanicBojan <zivanic.sv61.2021@uns.ac.rs>
2024-02-09 21:56:47 +0100

    #79 feat: add classes for filter strategy

---
+++ graph_visualizer_platform/src/graph_visualizer_platform/filter_strategies.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class FilterStrategy(ABC):
+    """
+    The FilterStrategy interface declares a method for evaluating if a given data
+    satisfies the filter criteria.
+    """
+
+    @abstractmethod
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        pass
+
+
+class EqualsFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) == attribute_value
+
+
+class NotEqualsFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) != attribute_value
+
+
+class GreaterThanFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) > attribute_value
+
+
+class LessThanFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) < attribute_value
+
+
+class GreaterThanOrEqualsFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) >= attribute_value
+
+
+class LessThanOrEqualsFilterStrategy(FilterStrategy):
+    def satisfies_filter(self, data: Any, attribute_value: str) -> bool:
+        return str(data) <= attribute_value
